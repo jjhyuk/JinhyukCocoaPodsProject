4f2a129b4e219a788f7d9e526ea72c5d99fecadd
jinhyuk <jjhyuk15@gmail.com>
2020-04-14 15:42:29 +0900

    Sample Cocoadpos

---
+++ JinhyukCocoaPodsProject/Classes/JHCocoaTest.h
@@ -0,0 +1,18 @@
+//
+//  JHCocoaTest.h
+//  Pods-JinhyukCocoaPodsProject_Tests
+//
+//  Created by jinhyuk on 2020/04/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JHCocoaTest : NSObject
+
++ (void) showLog:(NSString *) text;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JinhyukCocoaPodsProject/Classes/JHCocoaTest.m
@@ -0,0 +1,16 @@
+//
+//  JHCocoaTest.m
+//  Pods-JinhyukCocoaPodsProject_Tests
+//
+//  Created by jinhyuk on 2020/04/14.
+//
+
+#import "JHCocoaTest.h"
+
+@implementation JHCocoaTest
+
++ (void) showLog:(NSString *) text {
+    NSLog(@"JHCocoaTest : %@", text);
+}
+
+@end
